4dface99c963e572b70ef0ba3ec29f0820ea09ac
MatiasGalliani <matiasgalliani00@gmail.com>
2025-06-11 15:43:13 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -79,15 +79,20 @@ app.use(cors({
   origin: function(origin, callback) {
     const allowedOrigins = [
       'https://accelera.creditplan.it',
+      'https://accelera-ocosckkhc-matias-gallianis-projects.vercel.app',
+      'https://www.aimedici.it',
+      'http://www.aimedici.it',
       'http://localhost:5173',
       'http://localhost:3000'
     ];
     // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
+      log(logLevels.WARN, 'CORS request blocked', { origin });
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
+    log(logLevels.INFO, 'CORS request allowed', { origin });
     return callback(null, true);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
